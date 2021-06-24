622c508a2b27a03c1ecacebfda18c3f90cd491b8
Andrew Brampton <bramp@google.com>
2021-06-23 20:49:22 -0700

    Add support for left and right parens in the tokenizer.

--- src/zones/tokens.rs
+++ src/zones/tokens.rs
@@ -32,6 +32,12 @@ pub(crate) enum TokenType {
 
     /// TODO Add numbers? Add String Literals
 
+    /// Left / Open Parenthesis
+    LeftParen,
+
+    /// Right / Close Parenthesis
+    RightParen,
+
     /// A comment
     Comment,
 
@@ -60,15 +66,6 @@ pub(crate) struct Tokens<'a> {
     tokens: Vec<Token<'a>>,
 }
 
-pub fn line_ending<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
-where
-    T: Compare<&'static str>,
-    T: InputTake,
-    T: Clone + Offset + Slice<RangeTo<usize>>,
-{
-    tag("\n")(input)
-}
-
 pub fn comment<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
 where
     T: InputTakeAtPosition<Item = char>, // For take_while1
@@ -76,7 +73,7 @@ where
     T: InputTake,
     T: Clone + Offset + Slice<RangeTo<usize>>,
 {
-    // TODO This could be changed to take_until
+    // Always parses until the end of the line (or input).
     recognize(pair(tag(";"), take_while1(|c: char| c != '\n')))(input)
 }
 
@@ -91,20 +88,28 @@ pub fn word<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
 where
     T: InputTakeAtPosition<Item = char>, // For take_while1
 {
-    take_while1(|c: char| !c.is_whitespace())(input)
+    take_while1(|c: char| !c.is_whitespace() && c != '(' && c != ')' && c != '"')(input)
 }
 
 /// Take the input in turn into a sequence of tokens. This allows
 /// us to more easily handle comments, and records split across new lines.
 pub(crate) fn tokenise<'a, E: ParseError<Span<'a>> + ContextError<Span<'a>>>(
-    s: nom_locate::LocatedSpan<&'a str>,
+    s: LocatedSpan<&'a str>,
 ) -> Result<Tokens, E> {
     let ret = all_consuming(map(
         many0(
             // The token is one of:
             alt((
-                map(line_ending, |x| Token {
+                map(tag("\n"), |x| Token {
                     r#type: TokenType::LineEnding,
+                    pos: x,
+                }),
+                map(tag("("), |x| Token {
+                    r#type: TokenType::LeftParen,
+                    pos: x,
+                }),
+                map(tag(")"), |x| Token {
+                    r#type: TokenType::RightParen,
                     pos: x,
                 }),
                 // Comment starting with ;
@@ -222,60 +227,78 @@ impl InputLength for Tokens<'_> {
 mod tests {
     use super::*;
     use nom::error::VerboseError;
+    use pretty_assertions::assert_eq;
+    use std::ops::{RangeFrom, RangeTo};
 
-    fn print_err2<I: std::fmt::Debug, O: std::fmt::Debug>(
-        input: I,
-        err: IResult<I, O, VerboseError<I>>,
-    ) {
-        // If there is a error print a nice set of debugging.
-        println!("VerboseError: {:#?}", err);
-
-        /*
-        match err {
-            Err(Err::Error(e)) | Err(Err::Failure(e)) => {
-                println!(
-                    "VerboseError - `root::<VerboseError>(data)`:\n{}",
-                    convert_error(input, e)
-                );
-            }
-            _ => {}
+    impl Token<'_> {
+        fn new(r#type: TokenType, pos: Span) -> Token {
+            Token { r#type, pos }
         }
-        */
+    }
+
+    /// Simple type that takes a LocatedSpan, and makes it easy to break up
+    /// into its sub-spans.
+    struct EasySpan<T, X> {
+        input: LocatedSpan<T, X>,
+    }
+
+    impl<T, X> EasySpan<T, X>
+    where
+        LocatedSpan<T, X>: Slice<RangeFrom<usize>> + Slice<RangeTo<usize>>,
+    {
+        fn next(&mut self, count: usize) -> LocatedSpan<T, X> {
+            let (after, before) = self.input.take_split(count);
+            self.input = after;
+            before
+        }
     }
 
     #[test]
     fn test_tokenise() {
-        let input = Span::new(
-            // "$ORIGIN example.com.     ; designates the start of this zone file in the namespace
-            "VENERA  A       10.1.0.52",
-        );
-        tokenise::<VerboseError<LocatedSpan<&str>>>(input).unwrap();
+        let input = Span::new("VENERA  A      10.1.0.52 ; some comment");
 
-        /*
-        TODO
-                println!("{}", tokens);
+        let mut span = EasySpan { input };
+        let want = Tokens::new(&[
+            Token::new(TokenType::Word, span.next(6)),
+            Token::new(TokenType::Whitespace, span.next(2)),
+            Token::new(TokenType::Word, span.next(1)),
+            Token::new(TokenType::Whitespace, span.next(6)),
+            Token::new(TokenType::Word, span.next(9)),
+            Token::new(TokenType::Whitespace, span.next(1)),
+            Token::new(TokenType::Comment, span.next(14)),
+        ]);
+        let got = tokenise::<VerboseError<LocatedSpan<&str>>>(input).expect("token parsing failed");
+        assert_eq!(got, want);
+    }
 
-                let ret = parse_tokens::<VerboseError<Tokens>>(tokens.clone()); // TODO Avoid this clone!
-                if ret.is_err() {
-                    print_err2(tokens, ret);
-                    panic!("failed '{}'", input)
-                }
+    #[test]
+    fn test_tokenise_multi() {
+        let input = Span::new(concat!(
+            "@  IN  Action\\.domains (1\n",
+            "20      ; SERIAL\n",
+            "3600000); MINIMUM",
+        ));
 
-                let (remaining, got) = ret.unwrap();
-                //assert_eq!(got, want);
-                assert!(
-                    remaining.is_empty(),
-                    "all tokens should have been consumed."
-                );
-
-                panic!("blah");
-        */
-        /*
-        let position = output.unwrap().1.position;
-        assert_eq!(position.location_offset(), 14);
-        assert_eq!(position.location_line(), 2);
-        assert_eq!(position.fragment(), &"");
-        assert_eq!(position.get_column(), 2);
-        */
+        let mut span = EasySpan { input };
+        let want = Tokens::new(&[
+            Token::new(TokenType::Word, span.next(1)),
+            Token::new(TokenType::Whitespace, span.next(2)),
+            Token::new(TokenType::Word, span.next(2)),
+            Token::new(TokenType::Whitespace, span.next(2)),
+            Token::new(TokenType::Word, span.next(15)),
+            Token::new(TokenType::Whitespace, span.next(1)),
+            Token::new(TokenType::LeftParen, span.next(1)),
+            Token::new(TokenType::Word, span.next(1)),
+            Token::new(TokenType::LineEnding, span.next(1)),
+            Token::new(TokenType::Word, span.next(2)),
+            Token::new(TokenType::Whitespace, span.next(6)),
+            Token::new(TokenType::Comment, span.next(8)),
+            Token::new(TokenType::LineEnding, span.next(1)),
+            Token::new(TokenType::Word, span.next(7)),
+            Token::new(TokenType::RightParen, span.next(1)),
+            Token::new(TokenType::Comment, span.next(9)),
+        ]);
+        let got = tokenise::<VerboseError<LocatedSpan<&str>>>(input).expect("token parsing failed");
+        assert_eq!(got, want);
     }
 }
